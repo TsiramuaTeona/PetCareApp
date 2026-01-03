da81348a1cfebe74953fe5e97be1aa2c69c7fab5
TsiramuaTeona <teonatsiramua@gmail.com>
2026-01-03 15:01:31 +0400

    Initial Commit

---
+++ PetCareApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PetCareApp
+//
+//  Created by Teona Tsiramua on 03.01.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
