11595b09c2d49b1a0374df2eeeb04ecc6ab9fab6
TsiramuaTeona <teonatsiramua@gmail.com>
2026-01-09 23:20:03 +0400

    update: Image storage service

---
+++ PetCareApp/Services/ImageStorageService.swift
@@ -0,0 +1,46 @@
+//
+//  ImageStorageService.swift
+//  PetCareApp
+//
+//  Created by Teona Tsiramua on 08.01.26.
+//
+
+
+import Foundation
+import FirebaseStorage
+
+protocol ImageStorageServiceProtocol {
+    func uploadPetImage(petId: String, data: Data) async throws -> String
+    func uploadUserProfileImage(userId: String, data: Data) async throws -> String
+}
+
+final class ImageStorageService: ImageStorageServiceProtocol {
+
+    private let storage = Storage.storage().reference()
+
+    private func uploadImage(path: String, imageData: Data) async throws -> String {
+        let ref = storage.child(path).child("\(UUID().uuidString).jpg")
+
+        let metadata = StorageMetadata()
+        metadata.contentType = "image/jpeg"
+
+        _ = try await ref.putDataAsync(imageData, metadata: metadata)
+
+        let url = try await ref.downloadURL()
+        return url.absoluteString
+    }
+    
+    func uploadPetImage(petId: String, data: Data) async throws -> String {
+        try await uploadImage(
+            path: "pets/\(petId)",
+            imageData: data
+        )
+    }
+    
+    func uploadUserProfileImage(userId: String, data: Data) async throws -> String {
+        try await uploadImage(
+            path: "users/\(userId)",
+            imageData: data
+        )
+    }
+}
